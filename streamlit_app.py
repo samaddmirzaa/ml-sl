af84c59e8e034c8eac8f8bfdd0deca4f42b3cd44
Mirza Samad <abdul.samad197@gmail.com>
2026-07-21 15:37:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,16 +42,19 @@ data = {'island': island,
 input_df = pd.DataFrame(data, index=[0])
 input_penguins = pd.concat([input_df, X], axis=0)
 
+# Encode
+enode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+input_row = df_penguins[:1]
+
+
 with st.expander('Input Features'):
   st.write('**Input Penguin**')
   input_df
   st.write('**Combined Penguins Data**')
   input_penguins
-
- # Encode
-enode = ['island', 'sex']
-df_penguins = pd.get_dummies(input_penguins, prefix=encode)
-df_penguins[:1]
+  st.write('Encoded Input Penguin')
+ input_row
 
 
 
